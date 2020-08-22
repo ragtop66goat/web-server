a23e41f55160f47779f1d6231f0e476b3f3e1410
Jason Gionfriddo <ragtop66goat@gmail.com>
2020-08-21 22:07:53 -0500

    this all works

--- src/app.js
+++ src/app.js
@@ -1,21 +1,36 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
+const { response } = require('express')
 
-const publicDirectoryPath = path.join(__dirname, '../public')
 
 const app = express()
-app.set('view engine', 'hbs')
 
+//handlebars is the template engine. It expects the hbs files to live in the root directory in a directory called views
+//unless told to look elsewhere, hence the viewsPath var pointing to the tmplates file
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// set up handlebars as the engine and the views directory path
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+//setup static directory to serve up
 app.use(express.static(publicDirectoryPath))
 
+//the empty string is due to index being the default
 app.get('', (req, res) => {
+    //the first argument for render needs to match the hbs file
     res.render('index', {
 
-        title: 'Weather App', 
+        title: 'Weather', 
         name: 'Jason Gionfriddo'
     })
 })
 
+//the first argument mathces the webaddress for the desired page
 app.get('/weather', (req, res) => {
     res.send({
         forecast: 'It is snowing',
@@ -25,7 +40,7 @@ app.get('/weather', (req, res) => {
 
 app.get('/about', (req, res) => {
     res.render('about', {
-        title: 'About Me',
+        title: 'About Page',
         name: 'Jason Gionfriddo'
     })
 })
@@ -33,10 +48,30 @@ app.get('/about', (req, res) => {
 app.get('/help', (req, res) => {
     res.render('help', {
         title: 'Help Page',
-        message: 'This is where you can find the answers.'
+        helpText: 'This is where you can find the answers.',
+        name: 'Jason Gionfriddo'
     })
 })
 
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+       title: '404', 
+       name:'Jason Gionfriddo',
+       errorMessage: 'Help article not found' 
+    })
+
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404', 
+        name: 'Jason Gionfriddo',
+        errorMessage:'Page not found'
+    })
+
+})
+
+//this is what port your browser is looking for run this in the terminal with nodemon src/app.js
 app.listen(3000, () => {
     console.log('Server is starting on port 3000')
 })
